a9c2e10fb86ad2bd54fd10a53180df1a58124a57
Natalia Palej <palejnatalia@gmail.com>
2023-03-15 17:26:04 +0000

    Working on SAXParser

--- Project2023/src/project2023/SAX.java
+++ Project2023/src/project2023/SAX.java
@@ -1,21 +1,15 @@
-
 package project2023;
 
 import org.xml.sax.helpers.DefaultHandler;
 
 
-
 public class SAX extends DefaultHandler {
-
-    
-      
+     
     
     public static void main(String[] args) {
         
         
     }//end of main
-    
-    
 }//end of SAX class
 
 
