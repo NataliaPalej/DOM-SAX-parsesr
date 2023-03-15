47053f7b445dab06b5043b51ee506e219fa2d44b
Natalia Palej <palejnatalia@gmail.com>
2023-03-15 17:28:09 +0000

    Imported packages that are necessary

--- Project2023/src/project2023/SAX.java
+++ Project2023/src/project2023/SAX.java
@@ -1,6 +1,11 @@
 package project2023;
 
 import org.xml.sax.helpers.DefaultHandler;
+import java.util.ArrayList;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 
 
 public class SAX extends DefaultHandler {
