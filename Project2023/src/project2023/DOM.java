e201770e8bb5484f16743dabaf86e0cdf45bd6ce
Natalia Palej <palejnatalia@gmail.com>
2023-03-15 16:49:33 +0000

    Created ArrayList to store file and declared Document doc

--- Project2023/src/project2023/DOM.java
+++ Project2023/src/project2023/DOM.java
@@ -3,18 +3,26 @@ package project2023;
 /**
  * @author Natalia A00279259
  */
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 public class DOM {
-   
-    
-        
 
-  
-    public static void main(String[] args){
-    	
-        
-        
+    //create an array list to store the items in
+    ArrayList<Golf> golfMajors = new ArrayList<>();
+    //declare document
+    Document dom;
+
+    public static void main(String[] args) {
+
     }//end of main
-    
+
 }//end of class DOM
 
